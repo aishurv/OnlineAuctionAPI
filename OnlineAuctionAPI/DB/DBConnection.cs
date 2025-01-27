7854260d639176200364e679e2423283acea1e81
Laxmi Wandare <Laxmi.wandare@klingelenberg.com>
2025-01-27 15:44:07 +0530

    Add project files.

---
+++ OnlineAuctionAPI/DB/DBConnection.cs
@@ -0,0 +1,37 @@
+﻿using MongoDB.Driver;
+using DockerMongoTestApp.Models;
+namespace DB
+{
+    
+    public static class DBConnection
+    {
+        //public static MongoClient mongoClient = new MongoClient("mongodb://admin:password@mongo-container:27017");
+        public static MongoClient mongoClient = new MongoClient("mongodb://localhost:27017");
+        public static IMongoDatabase Database = mongoClient.GetDatabase("OnlineAuction");
+        private static readonly IMongoCollection<Product> _productCollection;
+
+        static DBConnection()
+        {
+            _productCollection = Database.GetCollection<Product>("products");
+            Console.WriteLine(Database.GetType().Name);
+        }
+        public static List<Product> Products
+        {
+            get
+            {
+                var collection = Database.GetCollection<Product>("products");
+                return collection.Find(_ => true).ToList();
+            }
+        }
+        public static void AddProduct(Product product)
+        {
+            _productCollection.InsertOne(product);
+        }
+        public static void AddProducts(List<Product> products)
+        {
+            _productCollection.InsertMany(products);
+        }
+
+
+    }
+}
---
+++ OnlineAuctionAPI/Model/Product.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson;
+
+namespace DockerMongoTestApp.Models
+{
+    public class Product
+    {
+        [BsonId]
+        public ObjectId Id { get; set; }
+        [BsonElement("name")]
+        public string Name { get; set; }
+        [BsonElement("description")]
+        public string Description { get; set; }
+        [BsonElement("price")]
+        public decimal Price { get; set; }
+    }
+}
